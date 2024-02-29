e58950ab0dfe58a94076df6da7da14bf236d0a3c
Brubs <Bruna_martins@outlook.com>
2024-02-29 16:32:41 -0300

    Criaçao da Classe Somar

--- src/br/edu/fatecpg/controleversao/Principal.java
+++ src/br/edu/fatecpg/controleversao/Principal.java
@@ -1,5 +1,11 @@
 package br.edu.fatecpg.controleversao;
 
 public class Principal {
-
+	
+	public static void main(String[] args) {
+	System.out.println("Olá Mundo");
+	
+	Somar calc = new Somar();
+	System.out.println(calc.soma(20, 10));
+	}
 }

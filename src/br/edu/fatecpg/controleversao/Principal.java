42211e6edb2a320a174ab0812d2736e257257543
Brubs <Bruna_martins@outlook.com>
2024-02-29 17:35:16 -0300

    Implementação da calculadora

--- src/br/edu/fatecpg/controleversao/Principal.java
+++ src/br/edu/fatecpg/controleversao/Principal.java
@@ -1,11 +1,36 @@
 package br.edu.fatecpg.controleversao;
+import java.util.Scanner;
 
 public class Principal {
 	
 	public static void main(String[] args) {
-	System.out.println("Olá Mundo");
-	
-	Somar calc = new Somar();
-	System.out.println(calc.soma(20, 10));
+		int operacao;
+		double num1, num2;
+    Scanner scan = new Scanner(System.in);
+    
+    while(true) {
+    	System.out.println("Digite a operação desejada: ");
+    	operacao = scan.nextInt();
+    	System.out.println("Digite seu primeiro valor: ");
+    	num1 = scan.nextDouble();
+    	System.out.println("Digite seu segundo valor: ");
+    	num2 = scan.nextDouble();
+    	
+    	switch(operacao) {
+    	case 1: 
+    		resultado = calc.soma(num1, num2);
+    		break;
+    	case 2: 
+    		resultado = calc.subtrai(num1, num2);
+    		break;
+    	case 3: 
+    		resultado = calc.multiplica(num1, num2);
+    		break;
+    	case 4: 
+    		resultado = calc.dividi(num1, num2);
+    		break;
+    	   	
+    	}
+    }
 	}
 }
